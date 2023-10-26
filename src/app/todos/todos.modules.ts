38767295b4136676948dd7f0943f3219d2fa5687
Cagdas Muldur <cagdasmuldur@gmail.com>
2023-10-26 11:28:00 -0400

    fix: header is added

---
+++ src/app/todos/todos.modules.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+
+import { TodosComponent } from 'src/app/todos/components/todos/todos.component';
+import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from 'src/app/todos/components/header/header.component';
+const routes: Routes = [
+  {
+    path: '',
+    component: TodosComponent,
+  },
+];
+
+@NgModule({
+  declarations: [TodosComponent, HeaderComponent],
+  imports: [RouterModule.forChild(routes)],
+})
+export class TodosModule {}
